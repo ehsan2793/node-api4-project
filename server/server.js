ce118a743b0fdb0d1643cf5f1ef5f7cd45dac98c
ehsan2793 <ehsan2793@gmail.com>
2021-09-30 13:28:23 -0700

    tes 4

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const useRouter = require('../routers/userRouter');
 server.use('/api', useRouter)
 
 server.get('/', (req, res) => {
-    res.send("<div><h1>hello wrold</h1><p>welcome to my api to start getting data please  add <strong>/api/users</strong>  to the end of your url</p><p>you can also click this button to take you there</p> <button><a href=/api/users>click here</a> </button></div>")
+    res.send("<div><h1>hello wrold</h1><p>welcome to my api to start getting data please  add <strong>/api/users</strong>  to the end of your url</p><p>you can also click this button to take you there</p> <button><a href=/users>click here</a> </button></div>")
 })
 
 
