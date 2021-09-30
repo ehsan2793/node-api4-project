3962e1da4caecbb8154677ed8fc41b2607f24fdb
ehsan2793 <ehsan2793@gmail.com>
2021-09-30 14:16:39 -0700

    posting the name will give us the password

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -34,7 +34,7 @@ router.post('/register', (req, res) => {
 router.post('/login', (req, res) => {
     let found = users.find(user => user.name === req.body.name)
     if (found) {
-        res.status(200).json(found)
+        res.status(200).json(found.password)
     } else {
         res.status(403).json({ message: 'did not find user' })
     }
